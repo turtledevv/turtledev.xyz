4bdfd23be61206529de53f697d2fde40c764186d
turtledevv <62922384+coolboy67yt@users.noreply.github.com>
2025-10-08 23:17:48 -0500

    Update script.js
    Add support for older web browsers

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
+
 function setCookie(name, value, days) {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+    document.cookie = name + "=" + value + "; expires=" + expires.toUTCString() + "; path=/";
 }
+
 
 function getCookie(name) {
-    return document.cookie.split('; ').find(row => row.startsWith(name + '='))?.split('=')[1];
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
 
+
 window.onload = function () {
-    const savedMode = getCookie('theme');
+    var savedMode = getCookie('theme');
     if (savedMode === 'dark') {
-    document.body.id = 'dark';
+        document.body.id = 'dark';
     }
     removeBlackBox();
 };
 
+
 function togglemode() {
     if (document.body.id === 'dark') {
-    document.body.id = '';
-    setCookie('theme', 'light', 365);
+        document.body.id = '';
+        setCookie('theme', 'light', 365);
     } else {
-    document.body.id = 'dark';
-    setCookie('theme', 'dark', 365);
+        document.body.id = 'dark';
+        setCookie('theme', 'dark', 365);
     }
 }
 
+
 function removeBlackBox() {
-    const box = document.getElementById("blackBox");
-    if (!box) {
-    return;
-    }
+    var box = document.getElementById("blackBox");
+    if (!box) return;
+
     box.style.opacity = "0";
-    box.addEventListener("transitionend", () => {
-    if (box.parentNode) {
-        box.remove();
-        }
-    });
+
+    var removed = false;
+    var cleanup = function() {
+        if (removed) return;
+        removed = true;
+        if (box.parentNode) box.parentNode.removeChild(box);
+    };
+
+    if (box.addEventListener) {
+        box.addEventListener("transitionend", cleanup, false);
+    }
+
+    /* in case shit fails, remove after 1s */
+    setTimeout(cleanup, 1000);
 }
