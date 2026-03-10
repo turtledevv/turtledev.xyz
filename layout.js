bdaf82cfdf6b6348d78762349bd892b027c6e76f
turtledevv <62922384+turtledevv@users.noreply.github.com>
2026-03-10 13:10:03 -0500

    Reverting all layout.js related changes to the first version
    I give up

--- layout.js
+++ layout.js
@@ -4,28 +4,17 @@
     const headerReq = fetch("/assets/header.html").then(r => r.text());
     const footerReq = fetch("/assets/footer.html").then(r => r.text());
 
-    // insert da header
     headerReq.then(html => {
-        const first = body?.firstElementChild;
+        const first = document.body?.firstElementChild;
 
         if (first && first.classList.contains("aboveHeader")) {
             first.insertAdjacentHTML("afterend", html);
         } else {
-            body.insertAdjacentHTML("afterbegin", html);
+            (document.body || body).insertAdjacentHTML("afterbegin", html);
         }
-
-        requestAnimationFrame(() => {
-            const currentPage = body.dataset.page;
-            if (!currentPage) return;
-
-            // use querySelector to handle cases where nav buttons are nested
-            const btn = document.querySelector(`#nav-${currentPage}`);
-            if (btn) btn.style.fontWeight = "bold";
-        });
     });
 
-    // then, insert thy footeh
     footerReq.then(html => {
-        body.insertAdjacentHTML("beforeend", html);
+        (document.body || body).insertAdjacentHTML("beforeend", html);
     });
 })();
